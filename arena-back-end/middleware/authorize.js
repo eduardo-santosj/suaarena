11fba8bc2aa80db4259796dfb7fea3bb16c4dba0
Edu Santos <eduardosantosj2@gmail.com>
2026-02-24 15:44:17 -0300

    Allow viewer to make GET requests but block POST/PUT/DELETE

--- arena-back-end/middleware/authorize.js
+++ arena-back-end/middleware/authorize.js
@@ -1,9 +1,20 @@
 const authorize = (allowedRoles) => {
   return (req, res, next) => {
     const userType = req.user?.userType;
+    const method = req.method;
     
     if (!userType) {
       return res.status(403).json({ error: 'Tipo de usuário não encontrado' });
+    }
+    
+    // Viewer pode fazer GET em qualquer rota
+    if (userType === 'viewer' && method === 'GET') {
+      return next();
+    }
+    
+    // Viewer não pode fazer POST, PUT, DELETE
+    if (userType === 'viewer' && ['POST', 'PUT', 'DELETE'].includes(method)) {
+      return res.status(403).json({ error: 'Acesso negado: usuário viewer não pode modificar dados' });
     }
     
     if (!allowedRoles.includes(userType)) {

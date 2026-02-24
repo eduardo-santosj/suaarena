2bb0fee7b5bcb1b02b47cbdbddce815447a540bf
Edu Santos <eduardosantosj2@gmail.com>
2026-02-24 11:44:13 -0300

    Remove sensitive files and update gitignore

--- gerar_hash_admin.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require('bcrypt');
-
-async function gerarHash() {
-  const senha = 'admin123';
-  const hash = await bcrypt.hash(senha, 10);
-  console.log(`INSERT INTO login (username, password, type) VALUES ('admin', '${hash}', 'admin');`);
-}
-
-gerarHash();

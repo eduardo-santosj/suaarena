42652754f50b037400a9361b6c66d0e8ca2c7f00
Edu Santos <eduardosantosj2@gmail.com>
2026-02-24 11:20:42 -0300

    Fix MySQL connection timeout using connection pool

--- arena-back-end/db.js
+++ arena-back-end/db.js
@@ -1,17 +1,21 @@
 require('dotenv').config();
 const mysql = require('mysql2');
 
-const db = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306
+  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0
 });
 
-db.connect(err => {
-  if (err) throw err;
-  console.log('Conectado ao MySQL');
+pool.on('connection', () => {
+  console.log('Nova conexão MySQL criada');
 });
 
-module.exports = db;
+module.exports = pool;
